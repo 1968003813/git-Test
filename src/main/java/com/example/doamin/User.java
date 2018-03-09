7b80e752266d664c9493b25f0d49b8dc52ded400
1968003813 <1968003813@qq.com>
2018-03-09 15:13:46 +0800

    My First Git  2018/3/9

---
+++ src/main/java/com/example/doamin/User.java
@@ -0,0 +1,22 @@
+package com.example.doamin;
+
+public class User {
+    private  Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User() {
+
+    }
+
+    public User(Integer id) {
+
+        this.id = id;
+    }
+}
